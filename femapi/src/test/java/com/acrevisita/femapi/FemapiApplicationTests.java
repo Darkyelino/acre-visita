8b2b041a852844e09997a2c2df01ebe8af8f8675
Pablo Cauã da Silva Vasques <abel.vasques2004@gmail.com>
2025-08-12 17:13:07 -0500

    feat: organização da documentação em uma pasta e criação e configuração do projeto spring boot java

---
+++ femapi/src/test/java/com/acrevisita/femapi/FemapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acrevisita.femapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FemapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
