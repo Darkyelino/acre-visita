ac8d4707215ceaec1356c78bc3bcfec1cdaf3430
Pedro <pedrosoaresdeassis@gmail.com>
2025-11-04 14:50:04 -0500

    Correção na visibilidade do botão de enviar email ao redefinir senha e ajustando horário padrão da aplicação para o fuso-horário de Rio Branco

--- femapi/src/main/java/com/acrevisita/femapi/FemapiApplication.java
+++ femapi/src/main/java/com/acrevisita/femapi/FemapiApplication.java
@@ -1,15 +1,23 @@
 package com.acrevisita.femapi;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class FemapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FemapiApplication.class, args);
-
 		System.out.println("Hello World");
+	}
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Rio_Branco"));
 	}
 
 }
